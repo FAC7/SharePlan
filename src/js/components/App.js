3602d1442eb39f017eba3f5da415cfb657f48ca2
Katherine Bowler <katherine.bowler@gmail.com>
2016-05-19 16:14:34 +0100

    Create button component

--- src/js/components/App.js
+++ src/js/components/App.js
@@ -1,8 +1,17 @@
 import React from 'react'
 import Header from './Header/index.js'
+// import { cyan500 } from 'material-ui/styles/colors'
+// import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
+// import getMuiTheme from 'material-ui/styles/getMuiTheme'
 import { Grid, Row, Col } from 'react-flexbox-grid'
 
 import '../../scss/style.scss'
+
+// const muiTheme = getMuiTheme({
+//   palette: {
+//     textColor: cyan500
+//   }
+// })
 
 export default class App extends React.Component {
   render () {
--- src/js/components/Button/index.js
+++ src/js/components/Button/index.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react'
 import RaisedButton from 'material-ui/RaisedButton'
 
+const style = {
+  margin: 12
+}
+
 export default class Button extends Component {
   render () {
     return (
-      <RaisedButton label={this.props.buttonName} />
+      <RaisedButton label={this.props.buttonName} primary={true} style={style} />
     )
   }
 }
