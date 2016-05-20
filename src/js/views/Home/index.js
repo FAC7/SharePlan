ea70afebe15d358a4c42c753460faf18450b997c
virginie <virginietrubiano@gmail.com>
2016-05-20 12:00:59 +0100

    remove flexbox-grid and material-ui, bring react-bootstrap grid

--- src/js/views/Home/index.js
+++ src/js/views/Home/index.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
-import { Grid, Row, Col } from 'react-flexbox-grid'
+import { Grid, Row, Col } from 'react-bootstrap'
 
-import Button from '../../components/Button/'
+import DefaultButton from '../../components/Button/'
 
 export default class Home extends Component {
   render () {
@@ -11,20 +11,20 @@ export default class Home extends Component {
           <Row>
             <Col lg={6} md={6} xs={12}>
               <Row>
-                <Col lgOffset={8} lg={4} mdOffset={8} md={4} xsOffset={2} xs={8} >
-                  <Button buttonName='Patient Login'/>
+                <Col lgOffset={8} lg={4} mdOffset={8} md={4} xsOffset={4} xs={4} >
+                  <DefaultButton buttonName='I am a Patient'/>
                 </Col>
               </Row>
             </Col>
             <Col lg={6} md={6} xs={12}>
-              <Col lg={4} md={4} xsOffset={2} xs={8}>
-                <Button buttonName='Clinician Login' />
+              <Col lg={4} md={4} xsOffset={4} xs={4}>
+                <DefaultButton buttonName='I am a Clinician' />
               </Col>
             </Col>
           </Row>
           <Row>
             <Col xs={12}>
-              <img className='big-logo' src='/img/logo.png' />
+              <img className='logo' src='/img/logo.png' />
             </Col>
           </Row>
         </Grid>
