56e9cca209e9b954ac60c15c044302f3da35b5e9
Katherine Bowler <katherine.bowler@gmail.com>
2016-05-20 17:03:32 +0100

    Add Login Modal into view

--- src/js/views/Home/index.js
+++ src/js/views/Home/index.js
@@ -1,10 +1,22 @@
 import React, { Component } from 'react'
-import { Link } from 'react-router'
 import { Grid, Row, Col } from 'react-bootstrap'
 
 import DefaultButton from '../../components/Button/'
+import Login from '../../components/ClientLogin/'
 
 export default class Home extends Component {
+  constructor () {
+    super()
+    this.state = {
+      showModal: false
+    }
+    this.toggleModal = this.toggleModal.bind(this)
+  }
+
+  toggleModal () {
+    this.setState({ showModal: !this.state.showModal })
+  }
+
   render () {
     return (
       <div>
@@ -19,9 +31,7 @@ export default class Home extends Component {
             </Col>
             <Col lg={6} md={6} xs={12}>
               <Col lg={4} md={4} xsOffset={4} xs={4}>
-                <Link to={'/cliniciandashboard'}>
-                  <DefaultButton buttonName='I am a Clinician' />
-                </Link>
+                <DefaultButton buttonName='I am a Clinician' handleClick={this.toggleModal}/>
               </Col>
             </Col>
           </Row>
@@ -31,6 +41,7 @@ export default class Home extends Component {
             </Col>
           </Row>
         </Grid>
+        <Login toggleModal={this.toggleModal} showModal={this.state.showModal}/>
       </div>
     )
   }
