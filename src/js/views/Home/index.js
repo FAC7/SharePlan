f7557532fac7dd1b968aa0471a85aa8b0969eaf0
Katherine Bowler <katherine.bowler@gmail.com>
2016-05-20 17:04:43 +0100

    Move modal to patient button

--- src/js/views/Home/index.js
+++ src/js/views/Home/index.js
@@ -25,13 +25,13 @@ export default class Home extends Component {
             <Col lg={6} md={6} xs={12}>
               <Row>
                 <Col lgOffset={8} lg={4} mdOffset={8} md={4} xsOffset={4} xs={4} >
-                  <DefaultButton buttonName='I am a Patient'/>
+                  <DefaultButton buttonName='I am a Patient' handleClick={this.toggleModal}/>
                 </Col>
               </Row>
             </Col>
             <Col lg={6} md={6} xs={12}>
               <Col lg={4} md={4} xsOffset={4} xs={4}>
-                <DefaultButton buttonName='I am a Clinician' handleClick={this.toggleModal}/>
+                <DefaultButton buttonName='I am a Clinician'/>
               </Col>
             </Col>
           </Row>
