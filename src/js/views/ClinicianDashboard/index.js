3db6253076d8403961bda75bc35b82ca6153fb0f
Ellie <eleanor.reem@hotmail.co.uk>
2016-05-20 09:22:19 +0000

    created the basic client list without functionality and styling

---
+++ src/js/views/ClinicianDashboard/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import ClientList from '../../components/ClinicianDashboard/ClientList/index.js'
+
+export default class ClinicianDashboard extends React.Component {
+
+  render () {
+
+    return (
+      <div>
+        <ClientList />
+      </div>
+    )
+  }
+}
---
+++ src/test/cliniciandashboard_test.js
@@ -0,0 +1,11 @@
+// var ClinicianDashboard = require('../js/views/ClinicianDashboard/')
+// const test = require('tape')
+// var ReactTestUtils = require('react-addons-test-utils')
+// import React from 'react'
+
+// test('ClinicianDashboard view', (t) => {
+//   const clinicanDashboard = <ClinicianDashboard test='testing' />
+//   ReactTestUtils.renderIntoDocument(clinicanDashboard)
+//   t.equal(clinicanDashboard.props.test, 'testing', 'Test prop exists')
+//   t.end()
+// })
