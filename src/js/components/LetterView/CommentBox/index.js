bffc2afc37e049ea2ad25d99a346c22d0265b4b8
Katherine Bowler <katherine.bowler@gmail.com>
2016-05-20 17:33:02 +0100

    Fix formatting & linter errors

--- src/js/components/LetterView/CommentBox/index.js
+++ src/js/components/LetterView/CommentBox/index.js
@@ -3,80 +3,85 @@ import {Button, FormControl, FormGroup, ControlLabel} from 'react-bootstrap'
 import CommentItem from '../CommentItem/index.js'
 
 export default class CommentBox extends React.Component {
-    constructor () {
-        super () 
-        this.state ={
-            commentBody: '',
-            comments: []
-        }
+  constructor () {
+    super()
+    this.state = {
+      commentBody: '',
+      comments: []
     }
-    componentWillMount () {
-        this.setState ({
-            comments: this.props.comments
-        })
+  }
+
+  componentWillMount () {
+    this.setState({
+      comments: this.props.comments
+    })
+  }
+
+  submitComment (e) {
+    e.preventDefault()
+    const comment = {
+      body: this.state.commentBody,
+      author: 'currentUser',
+      date: new Date()
     }
-    submitComment(e) {
-        e.preventDefault()
-        const comment = {
-            body: this.state.commentBody,
-            author: 'currentUser',
-            date: new Date()
-        }
-        console.log('this is comment', comment.body)
-        console.log('author', comment.author)
-        console.log('date', comment.date)
-        const commentList = this.state.comments
-        console.log(commentList)
-        commentList.unshift(comment)
-        this.setState({
-            comments: commentList
-        })
-    }
-    handleChange (e) {
-        this.setState({
-            commentBody: e.target.value
-        })
-    }
-    render () {
-        return (
-            <div>
-                
-                    <form>
-                        <FormGroup controlId="formControlsTextarea">
-                            <ControlLabel>Give us any feedback concerning this letter</ControlLabel>
-                            <FormControl componentClass="textarea" placeholder="write your comments here" onChange={this.handleChange.bind(this)}/>
-                        </FormGroup>
-                        <Button bsStyle='info' type="submit" onClick={this.submitComment.bind(this)}>
-                            Submit
-                        </Button>
-                    </form>
-                    {this.state.comments.map((comment, i)=> {
-                        return <CommentItem body={comment.body} author={comment.author} key={i} date={comment.date}/>
-                    })}
-            </div>
-        )
-    }
+    const commentList = this.state.comments
+    commentList.unshift(comment)
+    this.setState({
+      comments: commentList
+    })
+  }
+
+  handleChange (e) {
+    this.setState({
+      commentBody: e.target.value
+    })
+  }
+
+  render () {
+    return (
+      <div>
+        <form>
+          <FormGroup controlId='formControlsTextarea'>
+            <ControlLabel>Give us any feedback concerning this letter</ControlLabel>
+            <FormControl componentClass='textarea' placeholder='write your comments here' onChange={this.handleChange.bind(this)}/>
+          </FormGroup>
+          <Button bsStyle='info' type='submit' onClick={this.submitComment.bind(this)}>
+            Submit
+          </Button>
+        </form>
+        {this.state.comments.map((comment, i) => {
+          return <CommentItem body={comment.body} author={comment.author} key={i} date={comment.date}/>
+        })}
+      </div>
+    )
+  }
+}
+
+CommentBox.propTypes = {
+  comments: React.PropTypes.array
 }
 
 CommentBox.defaultProps = {
-    comments: [
-            {
-                body: 'asdfashdfgkjashdfjkahsgdfjk',
-                date: 'wed 13th march',
-                author: 'brian'
-            },{
-                body: 'asdfashdfgkjashdfjkahsgdfjk',
-                date: 'wed 13th march',
-                author: 'fred'
-            },{
-                body: 'asdfashdfgkjashdfjkahsgdfjk',
-                date: 'wed 13th march',
-                author: 'fred'
-            },{
-                body: 'asdfashdfgkjashdfjkahsgdfjk',
-                date: 'thur 14th march',
-                author: 'sarah'
-            }
-        
-        ]
+  comments: [
+    {
+      body: 'asdfashdfgkjashdfjkahsgdfjk',
+      date: 'wed 13th march',
+      author: 'brian'
+    },
+    {
+      body: 'asdfashdfgkjashdfjkahsgdfjk',
+      date: 'wed 13th march',
+      author: 'fred'
+    },
+    {
+      body: 'asdfashdfgkjashdfjkahsgdfjk',
+      date: 'wed 13th march',
+      author: 'fred'
+    },
+    {
+      body: 'asdfashdfgkjashdfjkahsgdfjk',
+      date: 'thur 14th march',
+      author: 'sarah'
+    }
+  ]
 }
--- src/js/components/LetterView/CommentItem/index.js
+++ src/js/components/LetterView/CommentItem/index.js
@@ -1,13 +1,19 @@
 import React from 'react'
 
 export default class CommentItem extends React.Component {
-    render () {
-        return (
-            <div className='commentItem'>
-            <h5> {this.props.author}</h5>
-            <p> {(this.props.date).toString().substring(0, 21)} </p>
-            <p> {this.props.body} </p>
-            </div>
-            )
-    }
+  render () {
+    return (
+      <div className='commentItem'>
+        <h5> {this.props.author}</h5>
+        <p> {(this.props.date).toString().substring(0, 21)} </p>
+        <p> {this.props.body} </p>
+      </div>
+    )
+  }
+}
+
+CommentItem.propTypes = {
+  author: React.PropTypes.string,
+  date: React.PropTypes.string,
+  body: React.PropTypes.string
 }
