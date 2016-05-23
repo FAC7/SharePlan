3f33aeeea1971cb9d053f6b2b371b6c0caa00407
Ellie <eleanor.reem@hotmail.co.uk>
2016-05-23 11:00:50 +0000

    fixing tabbed spaces

--- src/js/components/ClinicianDashboard/LetterTable/index.js
+++ src/js/components/ClinicianDashboard/LetterTable/index.js
@@ -2,45 +2,45 @@ import React from 'react'
 import {Table, thead, th, td, tr, tbody, DropdownButton, MenuItem, Button, ProgressBar} from 'react-bootstrap'
 
 export default class LetterTable extends React.Component {
-    render () {
-        return (
-            <Table responsive>
-                <thead>
-                    <tr>
-                    <th>ID</th>
-                    <th>Topic</th>
-                    <th>Recipients</th>
-                    <th>Correspondence</th>
-                    <th>Status</th>
-                    <th>Due</th>
+  render () {
+  	return (
+      <Table responsive>
+          <thead>
+              <tr>
+              <th>ID</th>
+              <th>Topic</th>
+              <th>Recipients</th>
+              <th>Correspondence</th>
+              <th>Status</th>
+              <th>Due</th>
+            </tr>
+          </thead>
+          <tbody>
+          {this.props.letters? this.props.letters.map((letter, i) => {
+              return (
+                  <tr key={i}>
+                      <td>{i}</td>
+                      <td>
+                          <Button bsStyle="link">
+                              {letter.topic}
+                          </Button>
+                      </td>
+                      <td>{letter.recipients}</td>
+                      <td>{letter.correspondence}</td>
+                      <td>
+                          <DropdownButton bsStyle='info' title={letter.status} id={`dropdown-basic-${i}`}>
+                            <MenuItem eventKey="1">On the list</MenuItem>
+                            <MenuItem eventKey="2">In preperation</MenuItem>
+                            <MenuItem eventKey="3" active>Waiting review</MenuItem>
+                            <MenuItem eventKey="4">Sent</MenuItem>
+                          </DropdownButton></td>
+                      <td>{letter.due}</td>
                   </tr>
-                </thead>
-                <tbody>
-                {this.props.letters? this.props.letters.map((letter, i) => {
-                    return (
-                        <tr key={i}>
-                            <td>{i}</td>
-                            <td>
-                                <Button bsStyle="link">
-                                    {letter.topic}
-                                </Button>
-                            </td>
-                            <td>{letter.recipients}</td>
-                            <td>{letter.correspondence}</td>
-                            <td>
-                                <DropdownButton bsStyle='info' title={letter.status} id={`dropdown-basic-${i}`}>
-                                  <MenuItem eventKey="1">On the list</MenuItem>
-                                  <MenuItem eventKey="2">In preperation</MenuItem>
-                                  <MenuItem eventKey="3" active>Waiting review</MenuItem>
-                                  <MenuItem eventKey="4">Sent</MenuItem>
-                                </DropdownButton></td>
-                            <td>{letter.due}</td>
-                        </tr>
-                    )
-                }) : ''}
-                </tbody>
-              </Table>
-            )
-    }
+              )
+          }) : ''}
+          </tbody>
+        </Table>
+      )
+  }
     
 }
