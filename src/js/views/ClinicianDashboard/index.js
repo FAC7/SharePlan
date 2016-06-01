394544a1b34dd6b71bb56068cd33a92a83add745
Katherine Bowler <katherine.bowler@gmail.com>
2016-06-01 12:30:26 +0100

    Clean up code
    Remove unnecessary comments and default props

--- src/js/views/ClinicianDashboard/index.js
+++ src/js/views/ClinicianDashboard/index.js
@@ -26,6 +26,7 @@ export default class ClinicianDashboard extends React.Component {
       browserHistory.push('/')
     }
   }
+
   componentDidMount () {
     this.getClients()
   }
@@ -37,41 +38,6 @@ export default class ClinicianDashboard extends React.Component {
       }
     })
     .then((response) => {
-      console.log('response from /get-all-patients-letters request, ClinicianDashboard line 39',
-      response)
-      console.log('clinician_id, ClinicianDashboard line 40', this.state.clinician_id)
-      // const realDataFormat = [ {
-      //   patient_id: 'jfewah8493',
-      //   first_name: 'Kat',
-      //   last_name: 'Bow',
-      //   topic: 'some topic',
-      //   recipient: 'Mum',
-      //   status: 'sent',
-      //   date_created: '2016-01-28'
-      // }, {
-      //   patient_id: 'jfewah8493',
-      //   first_name: 'Kat',
-      //   last_name: 'Bow',
-      //   topic: 'some topicssss',
-      //   recipient: 'Mum',
-      //   status: 'sent',
-      //   date_created: '2016-01-28'
-      // }, {
-      //   patient_id: 'jfewah9093',
-      //   first_name: 'Kit',
-      //   last_name: 'Bew',
-      //   topic: 'some topic',
-      //   recipient: 'Mum',
-      //   status: 'sent',
-      //   date_created: '2016-01-28'
-      // } ]
-
-      // const clientsObj = realDataFormat.reduce((clientObj, letter) => {
-      //   const id = letter.patient_id
-      //   clientObj[id] = clientObj[id] ? clientObj[id].concat(letter) : [ letter ]
-      //   return clientObj
-      // }, {})
-
       const clientsObj = response.data.reduce((clientObj, letter) => {
         const id = letter.patient_id
         clientObj[id] = clientObj[id] ? clientObj[id].concat(letter) : [ letter ]
@@ -108,73 +74,4 @@ export default class ClinicianDashboard extends React.Component {
 ClinicianDashboard.propTypes = {
   currentUser: React.PropTypes.string,
   clients: React.PropTypes.object
-}
-
-ClinicianDashboard.defaultProps = {
-  currentUser: 'katbow',
-  clients: {
-    54634563456: {
-      letters: [
-        {
-          topic: 'Assessment results',
-          recipient: 'School',
-          status: 'Sent',
-          date_created: '10/11/16'
-        }, {
-          topic: 'Assessment results',
-          recipient: 'School',
-          status: 'Waiting review',
-          date_created: '10/11/16'
-        },
-        {
-          topic: 'Assessment results',
-          recipient: 'School',
-          status: 'On the list',
-          date_created: '10/11/16'
-        }
-      ]
-    },
-    1324523452345: {
-      letters: [
-        {
-          topic: 'Assessment results',
-          recipient: 'School',
-          status: 'On the list',
-          date_created: '10/11/16'
-        }, {
-          topic: 'Assessment results',
-          recipient: 'School',
-          status: 'Sent',
-          date_created: '10/11/16'
-        },
-        {
-          topic: 'Assessment results',
-          recipient: 'School',
-          status: 'Waiting review',
-          date_created: '10/11/16'
-        }
-      ]
-    },
-    24356345766: {
-      letters: [
-        {
-          topic: 'Assessment results',
-          recipient: 'School',
-          status: 'Sent',
-          date_created: '10/11/16'
-        }, {
-          topic: 'Assessment results',
-          recipient: 'School',
-          status: 'On the list',
-          date_created: '10/11/16'
-        },
-        {
-          topic: 'Assessment results',
-          recipient: 'School',
-          status: 'Waiting review',
-          date_created: '10/11/16'
-        }
-      ]
-    }
-  }
 }
