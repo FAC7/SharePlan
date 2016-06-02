887a77421a2cd40f19defbfb77c2c3ea4f36111b
virginie <virginietrubiano@gmail.com>
2016-06-02 16:15:57 +0100

    add style to the patient dashboard

--- src/js/components/ClientDashboard/ClientLetterTable/index.js
+++ src/js/components/ClientDashboard/ClientLetterTable/index.js
@@ -15,91 +15,97 @@ export default class ClientLetterTable extends React.Component {
     return (
       <div>
         {this.props.activeLetters ?
-          <Table responsive>
-            <thead>
-              <tr>
-                <th>
-                  Topic
-                </th>
-                <th>
-                  Recipients
-                </th>
-                <th>
-                  Status
-                </th>
-              </tr>
-            </thead>
-            <tbody>
-              {this.props.activeLetters.map((letter, i) => {
-                return (
-                  <tr key={i}>
-                    <td>
-                      <Button bsStyle='link'>
-                        {letter.topic}
-                      </Button>
-                    </td>
-                    <td>
-                     {letter.recipient}
-                    </td>
-                    <td>
-                      <Row>
-                        <Col className='arrow-status' xs={2}>
-                          <p className={this.colorText(letter.status, 'On the list')}>
-                            On the list
-                          </p>
-                        </Col>
-                        <Col className='arrow-status' xs={2}>
-                          <p className={this.colorText(letter.status, 'In progress')}>
-                            In progress
-                          </p>
-                        </Col>
-                        <Col xs={1}>
-                          <Glyphicon className='arrow-right' glyph='chevron-right' />
-                        </Col>
-                        <Col xs={3}>
-                          <p className={this.colorText(letter.status, 'Sent')}>
-                            Sent
-                          </p>
-                        </Col>
-                      </Row>
-                    </td>
-                  </tr>
-                )})
-              }
-            </tbody>
-          </Table>
+          <div className='client-table'>
+            <Table responsive className='client-heading'>
+              <thead>
+                <tr>
+                  <th>
+                    Topic
+                  </th>
+                  <th>
+                    Recipients
+                  </th>
+                  <th>
+                    Status
+                  </th>
+                </tr>
+              </thead>
+              <tbody>
+                {this.props.activeLetters.map((letter, i) => {
+                  return (
+                    <tr key={i}>
+                      <td>
+                        <Button bsStyle='link'>
+                          {letter.topic}
+                        </Button>
+                      </td>
+                      <td>
+                       {letter.recipient}
+                      </td>
+                      <td>
+                        <Row>
+                          <Col className='arrow-status' xs={2}>
+                            <p className={this.colorText(letter.status, 'On the list')}>
+                              On the list
+                            </p>
+                          </Col>
+                          <Col className='arrow-status' xs={2}>
+                            <p className={this.colorText(letter.status, 'In progress')}>
+                              In progress
+                            </p>
+                          </Col>
+                          <Col xs={1}>
+                            <Glyphicon className='arrow-right' glyph='chevron-right' />
+                          </Col>
+                          <Col xs={3}>
+                            <p className={this.colorText(letter.status, 'Sent')}>
+                              Sent
+                            </p>
+                          </Col>
+                        </Row>
+                      </td>
+                    </tr>
+                  )})
+                }
+              </tbody>
+            </Table>
+          </div>
            : ''
          }
         {this.props.sentLetters ?
-          <Table responsive>
-            <thead>
-              <tr>
-                <th>
-                  Topic
-                </th>
-                <th>
-                  Recipients
-                </th>
-              </tr>
-            </thead>
-            <tbody>
+          <div className='client-table'>
+            <Table responsive className='client-heading'>
+              <thead>
+              <div>
+                <tr>
+                  <th>
+                    Topic
+                  </th>
+                  <th>
+                    Recipients
+                  </th>
+                </tr>
+                </div>
+              </thead>
+              <tbody>
 
-              {this.props.sentLetters.map((letter, i) => {
-                return (
-                  <tr key={i}>
-                    <td>
-                      <Button bsStyle='link'>
-                       {letter.topic}
-                      </Button>
-                    </td>
-                    <td>
-                      {letter.recipient}
-                    </td>
-                  </tr>
+                {this.props.sentLetters.map((letter, i) => {
+                  return (
+                    <tr key={i}>
+                      <td>
+                        <Button bsStyle='link'>
+                         {letter.topic}
+                        </Button>
+                      </td>
+                      <td>
+                        {letter.recipient}
+                      </td>
+                    </tr>
+                    )}
                   )}
-                )}
-            </tbody>
-          </Table> : ''}
+              </tbody>
+            </Table>
+          </div> : ''}
       </div>
     )
   }
